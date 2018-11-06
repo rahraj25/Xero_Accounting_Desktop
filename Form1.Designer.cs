9855f235a877bc24b11df370c7e656d839cd66f9
rahraj25 <rahu619@gmail.com>
2018-11-06 15:26:48 +1300

    design changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -59,18 +59,21 @@
             this.groupBox1.Controls.Add(this.btnIM);
             this.groupBox1.Controls.Add(this.btnAccounts);
             this.groupBox1.Controls.Add(this.btnDepotMngemnt);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(18, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(776, 426);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(1148, 537);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(489, 241);
+            this.label6.Location = new System.Drawing.Point(754, 256);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.Size = new System.Drawing.Size(69, 20);
             this.label6.TabIndex = 11;
             this.label6.Text = "Vehicles";
             // 
@@ -78,9 +81,10 @@
             // 
             this.btnTracking.BackgroundImage = global::CourierPro.Properties.Resources.map_marker;
             this.btnTracking.Enabled = false;
-            this.btnTracking.Location = new System.Drawing.Point(320, 278);
+            this.btnTracking.Location = new System.Drawing.Point(500, 313);
+            this.btnTracking.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnTracking.Name = "btnTracking";
-            this.btnTracking.Size = new System.Drawing.Size(124, 113);
+            this.btnTracking.Size = new System.Drawing.Size(186, 174);
             this.btnTracking.TabIndex = 10;
             this.btnTracking.UseVisualStyleBackColor = true;
             this.btnTracking.Click += new System.EventHandler(this.btnTracking_Click);
@@ -88,9 +92,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(366, 241);
+            this.label5.Location = new System.Drawing.Point(569, 256);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.Size = new System.Drawing.Size(69, 20);
             this.label5.TabIndex = 9;
             this.label5.Text = "Tracking";
             // 
@@ -99,9 +104,10 @@
             this.btnCars.BackgroundImage = global::CourierPro.Properties.Resources.truck;
             this.btnCars.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnCars.Enabled = false;
-            this.btnCars.Location = new System.Drawing.Point(450, 278);
+            this.btnCars.Location = new System.Drawing.Point(695, 313);
+            this.btnCars.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCars.Name = "btnCars";
-            this.btnCars.Size = new System.Drawing.Size(124, 113);
+            this.btnCars.Size = new System.Drawing.Size(186, 174);
             this.btnCars.TabIndex = 8;
             this.btnCars.UseVisualStyleBackColor = true;
             this.btnCars.Click += new System.EventHandler(this.btnCars_Click);
@@ -109,9 +115,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(123, 103);
+            this.label4.Location = new System.Drawing.Point(165, 118);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.Size = new System.Drawing.Size(70, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "Location";
             // 
@@ -132,9 +139,10 @@
             "Palmerston North",
             "Wellington - Johnsonville",
             "Wellington - Miramar"});
-            this.DepotLocations.Location = new System.Drawing.Point(215, 100);
+            this.DepotLocations.Location = new System.Drawing.Point(303, 114);
+            this.DepotLocations.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DepotLocations.Name = "DepotLocations";
-            this.DepotLocations.Size = new System.Drawing.Size(458, 21);
+            this.DepotLocations.Size = new System.Drawing.Size(685, 28);
             this.DepotLocations.TabIndex = 1;
             this.DepotLocations.SelectedIndexChanged += new System.EventHandler(this.DepotLocationSelected);
             this.DepotLocations.SelectionChangeCommitted += new System.EventHandler(this.DepotLocationSelected);
@@ -142,27 +150,30 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(588, 241);
+            this.label3.Location = new System.Drawing.Point(902, 256);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.Size = new System.Drawing.Size(172, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Inventory Management";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(228, 241);
+            this.label2.Location = new System.Drawing.Point(362, 256);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.Size = new System.Drawing.Size(76, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "Accounts";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 241);
+            this.label1.Location = new System.Drawing.Point(144, 256);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.Size = new System.Drawing.Size(151, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Depot Management";
             // 
@@ -171,9 +182,10 @@
             this.btnIM.BackgroundImage = global::CourierPro.Properties.Resources.settings;
             this.btnIM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnIM.Enabled = false;
-            this.btnIM.Location = new System.Drawing.Point(580, 278);
+            this.btnIM.Location = new System.Drawing.Point(890, 313);
+            this.btnIM.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnIM.Name = "btnIM";
-            this.btnIM.Size = new System.Drawing.Size(124, 113);
+            this.btnIM.Size = new System.Drawing.Size(186, 174);
             this.btnIM.TabIndex = 2;
             this.btnIM.UseVisualStyleBackColor = true;
             this.btnIM.Click += new System.EventHandler(this.btnIM_Click);
@@ -183,9 +195,10 @@
             this.btnAccounts.BackgroundImage = global::CourierPro.Properties.Resources.money;
             this.btnAccounts.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnAccounts.Enabled = false;
-            this.btnAccounts.Location = new System.Drawing.Point(190, 278);
+            this.btnAccounts.Location = new System.Drawing.Point(305, 313);
+            this.btnAccounts.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAccounts.Name = "btnAccounts";
-            this.btnAccounts.Size = new System.Drawing.Size(124, 113);
+            this.btnAccounts.Size = new System.Drawing.Size(186, 174);
             this.btnAccounts.TabIndex = 1;
             this.btnAccounts.UseVisualStyleBackColor = true;
             this.btnAccounts.Click += new System.EventHandler(this.btnAccounts_Click);
@@ -196,9 +209,10 @@
             this.btnDepotMngemnt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnDepotMngemnt.DialogResult = System.Windows.Forms.DialogResult.No;
             this.btnDepotMngemnt.Enabled = false;
-            this.btnDepotMngemnt.Location = new System.Drawing.Point(60, 278);
+            this.btnDepotMngemnt.Location = new System.Drawing.Point(110, 313);
+            this.btnDepotMngemnt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnDepotMngemnt.Name = "btnDepotMngemnt";
-            this.btnDepotMngemnt.Size = new System.Drawing.Size(124, 113);
+            this.btnDepotMngemnt.Size = new System.Drawing.Size(186, 174);
             this.btnDepotMngemnt.TabIndex = 0;
             this.btnDepotMngemnt.Text = "button1";
             this.btnDepotMngemnt.UseVisualStyleBackColor = true;
@@ -206,13 +220,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1189, 578);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Accounts";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
